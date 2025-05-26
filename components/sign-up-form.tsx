dba9c9f2b766c25dffe864dc75d8924747e41f2a
imikodanic <imikodanic@gmail.com>
2025-05-26 22:05:30 +0200

    fix: remove continue with google button

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {continueWithGoogle} from "@/app/login/actions";
 import Link from "next/link";
 import Image from "next/image";
 import {signup} from "@/app/sign-up/actions";
@@ -37,23 +36,23 @@ export function SignUpForm({
                             <div className="grid gap-2">
                                 <div className="flex items-center">
                                     <Label htmlFor="password">Password</Label>
-                                    <a
-                                        href="#"
-                                        className="ml-auto text-sm underline-offset-2 hover:underline"
-                                    >
-                                        Forgot your password?
-                                    </a>
+                                    {/*<a*/}
+                                    {/*    href="#"*/}
+                                    {/*    className="ml-auto text-sm underline-offset-2 hover:underline"*/}
+                                    {/*>*/}
+                                    {/*    Forgot your password?*/}
+                                    {/*</a>*/}
                                 </div>
                                 <Input id="password" name="password" type="password" required />
                             </div>
                             <Button type="submit" className="w-full" formAction={signup}>
                                 Sign Up
                             </Button>
-                            <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                  Or continue with
-                </span>
-                            </div>
+                            {/*<div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">*/}
+                            {/*    <span className="relative z-10 bg-background px-2 text-muted-foreground">*/}
+                            {/*      Or continue with*/}
+                            {/*    </span>*/}
+                            {/*</div>*/}
                             <div className="grid grid-cols-1 gap-4">
                                 {/*<Button variant="outline" className="w-full">*/}
                                 {/*    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">*/}
@@ -64,15 +63,15 @@ export function SignUpForm({
                                 {/*    </svg>*/}
                                 {/*    <span className="sr-only">Login with Apple</span>*/}
                                 {/*</Button>*/}
-                                <Button variant="outline" className="w-full" formAction={continueWithGoogle}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                        <path
-                                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                                            fill="currentColor"
-                                        />
-                                    </svg>
-                                    <span className="sr-only">Login with Google</span>
-                                </Button>
+                                {/*<Button variant="outline" className="w-full" formAction={continueWithGoogle}>*/}
+                                {/*    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">*/}
+                                {/*        <path*/}
+                                {/*            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"*/}
+                                {/*            fill="currentColor"*/}
+                                {/*        />*/}
+                                {/*    </svg>*/}
+                                {/*    <span className="sr-only">Login with Google</span>*/}
+                                {/*</Button>*/}
                                 {/*<Button variant="outline" className="w-full">*/}
                                 {/*    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">*/}
                                 {/*        <path*/}
@@ -103,7 +102,7 @@ export function SignUpForm({
                 </CardContent>
             </Card>
             <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-                By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+                By clicking sign up, you agree to our <a href="#">Terms of Service</a>{" "}
                 and <a href="#">Privacy Policy</a>.
             </div>
         </div>
