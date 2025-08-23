78be1ab928191fb91ecc519cdc32c51e2259f7cc
imikodanic <imikodanic@gmail.com>
2025-08-23 17:42:43 +0200

    feat: update oauth callback

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,22 +1,34 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+// The client you created from the Server-Side Auth instructions
 import { createClient } from "@/utils/supabase/server";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: Request) {
     const { searchParams, origin } = new URL(request.url);
     const code = searchParams.get("code");
-    const next = searchParams.get("next") || "/dashboard"; // fallback
+    // if "next" is in param, use it as the redirect URL
+    let next = searchParams.get("next") ?? "/";
+    if (!next.startsWith("/")) {
+        // if "next" is not a relative URL, use the default
+        next = "/";
+    }
 
     if (code) {
         const supabase = await createClient();
-        // This sets the auth cookies server-side
-        await supabase.auth.exchangeCodeForSession(code);
+        const { error } = await supabase.auth.exchangeCodeForSession(code);
+        if (!error) {
+            const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+            const isLocalEnv = process.env.NODE_ENV === "development";
+            if (isLocalEnv) {
+                // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+                return NextResponse.redirect(`${origin}${next}`);
+            } else if (forwardedHost) {
+                return NextResponse.redirect(`https://${forwardedHost}${next}`);
+            } else {
+                return NextResponse.redirect(`${origin}${next}`);
+            }
+        }
     }
 
-    // Optional: handle `error` param
-    const error = searchParams.get("error_description") || searchParams.get("error");
-    if (error) {
-        return NextResponse.redirect(new URL(`/login?error=${encodeURIComponent(error)}`, origin));
-    }
-
-    return NextResponse.redirect(new URL(next, origin));
+    // return the user to an error page with instructions
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
